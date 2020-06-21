782960304961a582f5f9baa535d2bedbac7f6ad7
kvalenty <krushunin89@gmail.com>
2020-06-21 18:08:02 +0300

    Updates

---
+++ precache-manifest.e5ac9e3bd7697ddcfb8e153de225585e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8545ad2c629bef3e0827b006158a94f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "86b996c2ca35d32e8841",
+    "url": "./static/css/main.9accec96.chunk.css"
+  },
+  {
+    "revision": "36689209a43d29ff1e8c",
+    "url": "./static/js/2.d4c7f416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d4c7f416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b996c2ca35d32e8841",
+    "url": "./static/js/main.0344cf44.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
